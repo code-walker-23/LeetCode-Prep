0bc06aa79cd6eab57b3fa1d1f6627ee20ce33b3f
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-09-12 20:55:01 +0530

    Time: 8 ms (77.09%), Space: 16.9 MB (44.8%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -62,3 +62,6 @@ public:
         }
     }
 };
+
+// Space Complexity -> O(1)
+// Time Complexity -> O((m+n)+(m-1*n-1)+(m-1*n-1)+(m+n)) -> O(2(m+n) + 2(m-1*n-1)) -> O(2(m-1*n-1)) -> O(m-1*n-1) -> O(m*n)
