1c9711c6cf4595e2aab73c4d0cb014dd41596931
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-09-21 11:16:41 +0530

    Time: 8 ms (52.88%), Space: 21.2 MB (85.78%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -14,9 +14,12 @@ public:
     //         nums[i] = expectedNums[i];
     //    }
     //    return expectedNums.size();
+
+    // In-place algo but O(n^2) 
     	int n = nums.size();
         int count = 1;
         int last = nums[0];
+        
         for(int i = 1; i < n; i++){
             if(nums[i] == last){
                 nums[i] = -101;
@@ -24,9 +27,7 @@ public:
                 last = nums[i];
                 count++;
             }
-            cout << nums[i] << " ";
         }
-        cout << endl;
 
         for(int i = 2; i < n; i++){
             if(nums[i] != -101){
