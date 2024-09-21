6cb7b50ac29c502d3cde4ba8d2cb6c30201e7b3f
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-09-21 09:34:51 +0530

    Time: 2 ms (45.33%), Space: 10.6 MB (12.47%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,24 +1,52 @@
 class Solution {
 public:
-    bool isSorted(vector<int> arr){
-        for(int i = 0; i < arr.size()-1; i++){
-            if(arr[i] > arr[i+1]){
-                return false;
-            }
-        }
-        return true;
-    }
+    // bool isSorted(vector<int> arr){
+    //     for(int i = 0; i < arr.size()-1; i++){
+    //         if(arr[i] > arr[i+1]){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    // bool check(vector<int>& nums) {
+    //     int n = nums.size();
+    //     int rotationNumber = 0;
+    //     int first = nums[0];
+        
+    //     for(int i = n-1; i >=2; i--){
+    //         if(first > nums[i]){
+    //             if(nums[i] == nums[i-1]){
+    //                 i--;
+    //             }
+    //             rotationNumber++;
+    //         }
+    //         else{
+    //             break;
+    //         }
+    //     }
+
+    //     vector<int> original(n);
+    //     for(int i = 0; i < n; i++){
+    //         original[(i+rotationNumber)%n] = nums[i];
+    //     }
+
+    //     return isSorted(original);
+
+    // }
 
     bool check(vector<int>& nums) {
-        int n = nums.size();
-        for(int x = 0; x < n; x++){
-            vector<int> rotated(n);
-            for(int j = 0; j < n; j++){
-                rotated[(j+x)%n] = nums[j];
+	    int n = nums.size();
+	    int count = 0;
+
+        for(int i = 0; i < n-1; i++){
+            if(nums[i] > nums[i+1]){
+                count++;
             }
-            if(isSorted(rotated)){
-                return true;
-            }
+        }
+
+        if(count == 0 || count == 1 && nums[0] >= nums[n-1]){
+            return true;
         }
 
         return false;
