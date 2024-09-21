766ab51c7400464409be944af60a94452769960d
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-09-21 11:14:16 +0530

    Time: 15 ms (12.31%), Space: 21.2 MB (96.96%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,44 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-       int n = nums.size();
-       vector<int> expectedNums;
-       expectedNums.push_back(nums[0]);
-       for(int i = 0; i < n-1; i++){
-            if(nums[i] != nums[i+1]){
-                expectedNums.push_back(nums[i+1]);
+    // out of place algo
+    //    int n = nums.size();
+    //    vector<int> expectedNums;
+    //    expectedNums.push_back(nums[0]);
+    //    for(int i = 0; i < n-1; i++){
+    //         if(nums[i] != nums[i+1]){
+    //             expectedNums.push_back(nums[i+1]);
+    //         }
+    //    }
+    //    for(int i = 0; i < expectedNums.size(); i++){
+    //         nums[i] = expectedNums[i];
+    //    }
+    //    return expectedNums.size();
+    	int n = nums.size();
+        int count = 1;
+        int last = nums[0];
+        for(int i = 1; i < n; i++){
+            if(nums[i] == last){
+                nums[i] = -101;
+            }else{
+                last = nums[i];
+                count++;
             }
-       }
-       for(int i = 0; i < expectedNums.size(); i++){
-            nums[i] = expectedNums[i];
-       }
-       return expectedNums.size();
+            cout << nums[i] << " ";
+        }
+        cout << endl;
+
+        for(int i = 2; i < n; i++){
+            if(nums[i] != -101){
+                for(int j = 1; j < i; j++){
+                    if(nums[j] == -101){
+                        swap(nums[j],nums[i]);
+                        break;
+                    }
+                }
+            }
+        }
+        return count;
     }
+
 };
