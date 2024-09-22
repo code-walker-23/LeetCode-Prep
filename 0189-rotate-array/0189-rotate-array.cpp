f0bd31e6b71af73dc01d29bf82ccfec426c70f6d
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-09-22 15:46:20 +0530

    Time: 27 ms (9.37%), Space: 27.6 MB (51.18%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -16,6 +16,25 @@ public:
         }
         cout << endl;
     }
+
+    // Optimal Solution Time -> O(n) ans Space -> O(1) 
+    void reverseArr(vector<int> &arr,int low,int high){
+    
+    // 	for(int i = 0; i < n/2; i++){
+    // 		int temp = arr[i];
+    // 		arr[i] = arr[n-i-1];
+    // 		arr[n-i-1] = temp;
+    // 	}
+    
+        while(low<high){
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    
+    }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         // 1st Sol
@@ -36,21 +55,25 @@ public:
 
         // 3rd Sol
         k = k%n;
-        vector<int> temp(k);
-        for(int i = n-k; i < n; i++){
-            temp[i-n+k] = nums[i];
-        }
-        printNums(temp);
+        // vector<int> temp(k);
+        // for(int i = n-k; i < n; i++){
+        //     temp[i-n+k] = nums[i];
+        // }
+        // printNums(temp);
 
-        for(int i = n-k-1; i >= 0; i--){
-            nums[i+k] = nums[i];
-        }
-        printNums(nums);
+        // for(int i = n-k-1; i >= 0; i--){
+        //     nums[i+k] = nums[i];
+        // }
+        // printNums(nums);
 
-        for(int i = 0; i < k; i++){
-            nums[i] = temp[i];
-        }
-        printNums(nums);
+        // for(int i = 0; i < k; i++){
+        //     nums[i] = temp[i];
+        // }
+        // printNums(nums);
+        if(k == 0)return;
+        reverseArr(nums,0,n-1);
+        reverseArr(nums,0,k-1);
+        reverseArr(nums,k,n-1);
 
     }
 };
