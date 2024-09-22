dcd8b0f1a0ab8d849e4c797f811a05d2fb89b583
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-09-22 20:22:04 +0530

    Time: 248 ms (5.04%), Space: 22 MB (28.14%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -21,6 +21,8 @@ public:
         // for(int i = 0; i < n; i++){
         //     nums[i] = res[i];
         // }
+
+        // 2nd Sol Time -> O(n^2) when all are zeroes and Space -> O(1)
         int count = 0;
         for(int i = 0; i < n; i++){
             if(nums[i] == 0){
