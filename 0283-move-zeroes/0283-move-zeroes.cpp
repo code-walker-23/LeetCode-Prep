1167d2b2178789c7462c65e044bef523505b839f
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-09-22 20:20:57 +0530

    Time: 238 ms (5.04%), Space: 21.9 MB (28.14%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,25 +3,41 @@ public:
     void moveZeroes(vector<int>& nums) {
         // Naive Approach Time -> O(n) and Space -> O(n)=This is the issue
         int n = nums.size();
+        // int count = 0;
+        // vector<int> res;
+        // for(int i = 0; i < n; i++){
+        //     if(nums[i] == 0){
+        //         count++;
+        //     }
+        //     else{
+        //         res.push_back(nums[i]);
+        //     }
+        // }
+
+        // for(int i = n-count; i < n; i++){
+        //     res.push_back(0);
+        // }
+
+        // for(int i = 0; i < n; i++){
+        //     nums[i] = res[i];
+        // }
         int count = 0;
-        vector<int> res;
         for(int i = 0; i < n; i++){
             if(nums[i] == 0){
                 count++;
             }
-            else{
-                res.push_back(nums[i]);
+        }
+        
+        if(count == n)return;
+
+        for(int i = 1; i <= count; i++){
+            for(int j = 0; j < n-1; j++){
+                if(nums[j] == 0 and nums[j+1] != 0){
+                    nums[j] = nums[j+1];
+                    nums[j+1] = 0;
+                }
             }
         }
 
-        for(int i = n-count; i < n; i++){
-            res.push_back(0);
-        }
-
-        for(int i = 0; i < n; i++){
-            nums[i] = res[i];
-        }
-
-        
     }
 };
