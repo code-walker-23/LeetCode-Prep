619cd873695332ab83738747c017849d8d52d3f7
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-09-30 00:18:50 +0530

    Time: 3 ms (47.55%), Space: 7.5 MB (79.94%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -2,13 +2,11 @@ class Solution {
     #define M 1000000007;
 public:
     long long myPow(int x, long long n) {
-        // x^0 -> becomes 1
         if(n == 0){
             return 1;
         }    
         long long temp = myPow(x,n/2);
         temp = (temp*temp)%M;
-        // if x is odd then -> x^37 -> x^18 * x^18 * x 
         if(n%2){
             return (temp*x)%M;
         }else{
