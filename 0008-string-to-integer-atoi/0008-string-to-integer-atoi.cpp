746d8771a117073ad8b62b70936f26188416debf
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-09-29 14:57:04 +0530

    Time: 7 ms (17.99%), Space: 10.9 MB (5.14%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -37,47 +37,37 @@ public:
         int n = s.size();
         long long num = 0;
         char sign = '+';
-        int i = 0;
+        bool flag = true;
+        bool isConsec = true;
+        int leadingZero = 0;
 
-        for(i; i < n; i++){
-            if(s[i] != ' '){
-                break;
-            }
-        }
-        cout << i << endl;
-        if(s[i] == '-'){
-            sign = '-';
-            cout << s[i] << endl;
-            i++;
-        }else if(s[i] == '+'){
-            i++;
-        }
-        cout << "sign -> " << sign << endl; 
-        set<char> se = {'0','1','2','3','4','5','6','7','8','9'};
-        for(i; i < n; i++){
-            if(se.count(s[i]) == 0){
+
+        unordered_set<char> se = {'0','1','2','3','4','5','6','7','8','9'};
+        for(int i = 0; i < n; i++){
+            if(s[i] == ' ' && isConsec){
+            }else if((s[i] == '+' || s[i] == '-') && flag && !leadingZero && !num){
+                sign = s[i];
+                flag = false;
+                isConsec = false;
+            }else if(se.count(s[i]) == 0){
                 break;
             }else{
+              isConsec = false;
               if(s[i] == '0' && num == 0){
-
+                leadingZero++;
               }else{
                 int x = charToInt(s[i]);
-                cout << x << endl;
-                cout << i << " -> " << s[i] << " -> " << x << endl;
                 num = (num*10) + x;
                 if(sign == '-' && (num*-1) <= INT_MIN){
                     num = INT_MIN;
-                    cout << "hoo" << endl;
                     break;
                 }else if(sign == '+' && num >= INT_MAX ){
                     num = INT_MAX;
-                    cout << "Hello";
                     break;
                 }
               } 
             }
         }
-        cout << num << endl;
         return sign == '+' ? num : num*-1;
     }
 };
