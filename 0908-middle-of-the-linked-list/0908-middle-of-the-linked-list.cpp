113dbd4b4ef82eb88157060f95fa29fbb7ba4e76
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-11-14 22:34:05 +0530

    Time: 0 ms (100%), Space: 9.4 MB (20.13%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -1,3 +1,55 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         // naive solution
+//         // int length = 1;
+//         // ListNode* temp = head;
+
+//         // while(temp->next != NULL){
+//         //     temp = temp->next;
+//         //     length++;
+//         // }
+//         // cout << length << endl;
+
+//         // int middle = length/2+1;
+//         // temp = head;
+//         // int currPos = 1;
+//         // // when curr position becomes equals to middle it loops break
+//         // while(currPos < middle){
+//         //     temp = temp->next;
+//         //     currPos++;
+//         // }
+//         // return temp;
+
+//         if(head->next == NULL){
+//             return head;
+//         }
+
+//         ListNode* slow = head;
+//         ListNode* fast = head->next;
+
+//         while(fast != NULL && fast->next != NULL){
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+//         // that means it has even length then we have to return the second largest because slow return the first middle we have to check for even length -> while breaks the last postion -> lastNode->next == NULL -> breaks for even length
+//         if(fast != NULL){
+//             return slow->next;
+//         }
+//         return slow;
+
+//     }
+// };
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -11,42 +63,14 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        // naive solution
-        // int length = 1;
-        // ListNode* temp = head;
-
-        // while(temp->next != NULL){
-        //     temp = temp->next;
-        //     length++;
-        // }
-        // cout << length << endl;
-
-        // int middle = length/2+1;
-        // temp = head;
-        // int currPos = 1;
-        // // when curr position becomes equals to middle it loops break
-        // while(currPos < middle){
-        //     temp = temp->next;
-        //     currPos++;
-        // }
-        // return temp;
-
-        if(head->next == NULL){
-            return head;
-        }
-
         ListNode* slow = head;
-        ListNode* fast = head->next;
+        ListNode* fast = head;
 
         while(fast != NULL && fast->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
         }
-        // that means it has even length then we have to return the second largest because slow return the first middle we have to check for even length -> while breaks the last postion -> lastNode->next == NULL -> breaks for even length
-        if(fast != NULL){
-            return slow->next;
-        }
-        return slow;
 
+        return slow;
     }
 };
