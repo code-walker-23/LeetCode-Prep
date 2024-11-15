2caf3e066e4d79c842cd0372390ac8dab69aefbc
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-11-15 13:42:27 +0530

    Time: 8 ms (67.9%), Space: 10.6 MB (96.51%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -1,3 +1,31 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         // brute force
+//         ListNode* curr = head;
+//         unordered_set<ListNode*> visited;
+
+//         while(curr != NULL){
+//             if(visited.find(curr) != visited.end())
+//                 return true;
+//             else{    
+//                 visited.insert(curr);
+//                 curr = curr->next;
+//             }
+//         }
+
+//         return false;
+//     }
+// };
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
