7840c0f90d7a71ee37e585e6ff8b4178245de75f
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-11-15 13:41:07 +0530

    Time: 16 ms (10.78%), Space: 14.6 MB (13.17%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,19 +10,15 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         // brute force
-        ListNode* prev = NULL;
         ListNode* curr = head;
         unordered_set<ListNode*> visited;
-        visited.insert(curr);
 
         while(curr != NULL){
-            prev = curr;
-            curr = curr->next;
-            if(curr != NULL){
-                if(visited.find(curr) != visited.end())
-                    return true;
-                else    
-                    visited.insert(curr);
+            if(visited.find(curr) != visited.end())
+                return true;
+            else{    
+                visited.insert(curr);
+                curr = curr->next;
             }
         }
 
