7f5e5ad6b7282314a8089e06fc728ac0db763c80
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-11-26 13:38:23 +0530

    Time: 3 ms (30.55%), Space: 15.6 MB (8.05%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
+// Time Complexity -> generating all the subsets of set -. 2^n -> O(2^n)
+// Space Complexity -> maximum height, at most i == n -> O(n) 
      void generateAllSubsets(vector<int>v,vector<vector<int>> &res,int i,vector<int>&nums){
     if(i == nums.size()){
         res.push_back(v);
         return;
     }
-
     // including the element
     v.push_back(nums[i]);
     generateAllSubsets(v,res,i+1,nums);
     // excluding the element
     v.pop_back();
     generateAllSubsets(v,res,i+1,nums);
-
  } 
  
  vector<vector<int>> subsets(vector<int>& nums) {
