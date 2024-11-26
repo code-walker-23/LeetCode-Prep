ddf5a779fcade5999d0d604701c5620781c3e525
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-11-26 12:05:19 +0530

    Time: 2 ms (83.64%), Space: 15 MB (69.43%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+// Two cases : OB >= CB during the recursive call
+// we will never get that case OB = CB = n; when we reach OB=n, then OB>CB , then we will take remaining closing braces and then push the string of required n pairs of parenthesis.
+// Time Complexity Analysis : Depends on How many well formed parentheses we can generate from n pair of parenthesis.
+// that is catalan number which (2n)!/(n+1)!*n!.
+
+// Space Complexity -> mazimum depth will be n -> O(n). 
+
         void generateParenthesis(int openingBraces,int closingBraces,int n,vector<string> &res,string s){
         if(openingBraces == n){
             while(closingBraces < n){
@@ -17,6 +24,7 @@ public:
     vector<string> generateParenthesis(int n) {
         vector<string> res;
         generateParenthesis(0,0,n,res,"");
+        // cout << res.size() << endl;
         return res;
     }
 };
