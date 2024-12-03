1ee0dab52d70893e28f8112e7025bc47453dd342
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-04 00:02:56 +0530

    Time: 0 ms (100%), Space: 59.3 MB (6.04%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
@@ -11,29 +11,39 @@
  */
 class Solution {
 public:
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-        TreeNode* newNode = new TreeNode(val);
-        TreeNode* dummyRoot = root;
-        if(root == NULL) return newNode;
-        
-        while(true){
-            if(dummyRoot->val < val){
-                if(dummyRoot->right == NULL){
-                    dummyRoot->right = newNode;
-                    break;
-                }else{
-                    dummyRoot = dummyRoot->right;
-                }
-            }else{
-                if(dummyRoot->left == NULL){
-                    dummyRoot->left = newNode;
-                    break; 
-                }else{
-                    dummyRoot = dummyRoot->left;
-                }
-            }
-        }
+// Time Complexity -> O(H) -> H is height of bst , max H will be in left or right skewed bst ,H = n-1, n is number of nodes and element to be inserted is min or max in the whole bst   
+// iterative -> Space -> O(1)
+    // TreeNode* insertIntoBST(TreeNode* root, int val) {
+    //     TreeNode* newNode = new TreeNode(val);
+    //     TreeNode* dummyRoot = root;
+    //     if(root == NULL) return newNode;
 
-        return root;
+    //     while(true){
+    //         if(dummyRoot->val < val){
+    //             if(dummyRoot->right == NULL){
+    //                 dummyRoot->right = newNode;
+    //                 break;
+    //             }else{
+    //                 dummyRoot = dummyRoot->right;
+    //             }
+    //         }else{
+    //             if(dummyRoot->left == NULL){
+    //                 dummyRoot->left = newNode;
+    //                 break; 
+    //             }else{
+    //                 dummyRoot = dummyRoot->left;
+    //             }
+    //         }
+    //     }
+
+    //     return root;
+    // }
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+       if(root == NULL) return new TreeNode(val);
+
+       if(root->val < val) root->right = insertIntoBST(root->right,val);
+       else root->left = insertIntoBST(root->left,val);
+
+       return root;
     }
 };
