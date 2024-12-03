4bc8cd8f7fe9590e807dfa5f96b63bf540ef61d7
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-03 11:31:18 +0530

    Time: 0 ms (100%), Space: 35.5 MB (6.11%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
@@ -11,18 +11,30 @@
  */
 class Solution {
 public:
-    TreeNode* SearchBST(TreeNode* root,int value){
-        if(root == NULL)return root;
-        if(root->val == value)return root;
-        if(value < root->val)return SearchBST(root->left,value);
-        return SearchBST(root->right,value);
-    }
+    // beat case complexity -> O(1)
+    // worst case complexity -> O(height of the bst is n-1, where n is the node and in the case of left skewed tree)
+    // space comp -> O(n)
+
+    // TreeNode* searchBST(TreeNode* root, int val) {
+    //    if(root ==  NULL || root -> val == val)return root;
+    //    // optimaztion  
+    //    if(root->val < val) 
+    //        return searchBST(root -> right,val);
+    //     return searchBST(root -> left,val);
+
+    // }
+
+    // iterative
+    // time complexity will be same of the recursive
+    // space -> O(1)
     TreeNode* searchBST(TreeNode* root, int val) {
-      while(root !=  NULL){
-          if(root->val == val)return root;
-          else if(root->val < val)root = root->right;
-          else root = root->left;
-      }
-        return NULL;
+
+       while(root != NULL){
+        if(root -> val == val)return root;
+        if(root -> val < val) root = root->right;
+        else root = root -> left;
+       }
+
+       return NULL;
     }
 };
