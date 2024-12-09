0b5c3c342f9679e6b58a2ed2895399526d3122a0
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-09 22:43:14 +0530

    Time: 0 ms (100%), Space: 24.3 MB (17.23%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -10,20 +10,44 @@
  * };
  */
 class Solution {
+    // Brute Solution -> Take dfs or bfs and store in the vector then O(bfs or dfs + sorting) = O(n+e+nlogn) ans space -> O(n)
+    // Better
     // Time Complexity -> O(n) -> inorder Traversal 
     // Space Complexity -> O(n) -> we will try to omit this
+
+    // Optimized
 public:
-    void inorderTraversal(TreeNode* root, vector<int>&v){
+    
+    // void inorderTraversal(TreeNode* root, vector<int>&v){
+    //     if(root == NULL)return;
+
+    //     inorderTraversal(root->left,v);
+    //     v.push_back(root->val);
+    //     inorderTraversal(root->right,v);
+    // }
+    // int kthSmallest(TreeNode* root, int k) {
+    //     vector<int> v;
+    //     inorderTraversal(root,v);
+
+    //     return v[k-1];
+    // }
+
+    // optimized space->O(1)
+    int ans;
+    void inorderTraversal(TreeNode* root, int &k){
+        if(k <= 0)return;
         if(root == NULL)return;
 
-        inorderTraversal(root->left,v);
-        v.push_back(root->val);
-        inorderTraversal(root->right,v);
+        inorderTraversal(root->left,k);
+        k--;
+        if(k == 0){
+            ans = root->val;
+        }
+        inorderTraversal(root->right,k);
     }
-    int kthSmallest(TreeNode* root, int k) {
-        vector<int> v;
-        inorderTraversal(root,v);
-        
-        return v[k-1];
+    int kthSmallest(TreeNode* root, int k) { 
+        inorderTraversal(root,k);
+
+        return ans;
     }
 };
