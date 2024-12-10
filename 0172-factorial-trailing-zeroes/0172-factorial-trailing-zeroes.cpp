627c14e09b1b89305b9e5d84149f223e2f34c341
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-10 20:40:47 +0530

    Time: 0 ms (100%), Space: 8.3 MB (8.56%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+// revision
+// Time Complexity -> O(factorial+4) -> O(n=10^4) -> sign overflow
+// Space -> O(1)
+//  but challenge is logn
+
+// count 2's and 5's number in factorization
+    // int trailingZeroes(int n) {
+    //     long long fact = 1;
+    //     for(int i = 1; i <= n; i++){
+    //         fact*=i;
+    //     }
+    //     int count = 0;
+    //     while(fact%10 == 0){
+    //         count++;
+    //         fact/=10;
+    //     }
+    //     return count;
+    // } 
+
+    int trailingZeroes(int n) {
+        int twosCount = 0;
+        int fivesCount = 0;
+
+        for(int i = 1; i <= n; i++){
+            int num = i;
+            while(num%2 == 0){
+                twosCount++;
+                num/=2;
+            }
+            while(num%5 == 0){
+                fivesCount++;
+                num/=5;
+            }
+        }
+
+        return min(twosCount,fivesCount);
+
+    } 
+};
