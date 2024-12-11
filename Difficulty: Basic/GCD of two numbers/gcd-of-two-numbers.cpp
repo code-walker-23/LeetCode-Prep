a32a2abe14c3018c84f9994fff98360bc38e0c13
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-11 13:15:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
@@ -8,19 +8,23 @@ using namespace std;
 class Solution {
   public:
     int gcd(int a, int b) {
+        if(b == 0)return a;
     //   for(int i = min(a,b); i >= 1; i++){
     //       if(a%i == 0 && b%i == 0){
     //           return i;
     //       }
     //   }
-    
-        while(a%b != 0){
-            int rem = a%b;
-            a = b;
-            b = rem;
-        }
+    // This is the traditional way of finding the gcd of two numbers
+    // Let a = 17,  b = 13
+    // gcd(17,13) ==> gcd(13,17%13) ==> gcd(13,4) ==> gcd(4,13%4) ==> gcd(4,1) ==> gcd(1,4%1) ==> gcd(1,0) ==> divisor becomes zero then the hcf is dividend a    
+    // we can also do this with recursion    
+        // while(a%b != 0){
+        //     int rem = a%b;
+        //     a = b;
+        //     b = rem;
+        // }
         
-        return b;
+        return gcd(b,a%b);
     }
 };
 
