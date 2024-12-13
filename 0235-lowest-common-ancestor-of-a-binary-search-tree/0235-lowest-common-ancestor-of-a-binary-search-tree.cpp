231d4156cb6277e9104110d53295d8c06d49e491
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-13 17:56:27 +0530

    Time: 20 ms (78.64%), Space: 23.4 MB (15.19%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,6 +10,9 @@
 
 class Solution {
 public:
+//Time Complexity -> O(height) -> O(n , when we have left or right skewwed tree)
+// Best -> when the tree is complete -> O(logn)
+// Space Compelxity -> O(1)
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(((p->val < root->val) && (q->val < root->val))){
             return lowestCommonAncestor(root->left,p,q);
@@ -17,6 +20,7 @@ public:
         if(((p->val > root->val)&&(q->val > root->val))){
             return lowestCommonAncestor(root->right,p,q);
         }
+        // when there is point of split -> either one them will be lesser than root and one will be greater than root or one of them will be equal to root
         return root;
     }
 };
