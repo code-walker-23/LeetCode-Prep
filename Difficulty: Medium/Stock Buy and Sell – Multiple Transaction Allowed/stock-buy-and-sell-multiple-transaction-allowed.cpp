8b4e21ba8b0268fcc6dabf95a877306f111cb94a
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-14 23:59:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock Buy and Sell – Multiple Transaction Allowed/stock-buy-and-sell-multiple-transaction-allowed.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int maximumProfit(vector<int> &arr) {
+        int maxPro = 0;
+        
+        // looking to sell
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] > arr[i-1]){
+                maxPro += (arr[i]-arr[i-1]);
+            }
+        }
+        // Using the graph intuition
+        return maxPro;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        int res = ob.maximumProfit(arr);
+        cout << res;
+
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
