3dd8631bd8c1da2e43cc9cb5f3df0a6ae97cfdcd
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-16 19:52:24 +0530

    Time: 0 ms (100%), Space: 13.5 MB (28.37%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
+    // when target is not present , at the last iteration of the our while, when low == high == mid
+    // from here there will be two cases
+    // 1.) arr[mid] > target ==> high = mid-1;   ==> low > high ==> break;
+    // 2.) arr[mid] < target ==> low = mid+1;    ==> low > high ==> break;
+    // low will be the insert pos
+
+    int searchInsert(vector<int>& arr, int target) {
         int low = 0;
-        int high = nums.size() -1;
+        int high = arr.size()-1;
+        int mid = -1;
+
         while(low <= high){
-            int mid = low + (high - low)/2;
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] > target)
-                high = mid -1;
-            else 
-                low = mid + 1;
+            mid = low + (high-low)/2;
+            if(arr[mid] == target)return mid;
+
+            if(arr[mid] < target){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
         }
-        if(low > high)return low;
-        else return high;
-        
+        return low;   
     }
 };
