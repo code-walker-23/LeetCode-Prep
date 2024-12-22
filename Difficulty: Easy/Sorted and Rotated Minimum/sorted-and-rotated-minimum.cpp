306f91f171e7b70140f729b8083d2dc50848f043
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-22 21:37:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.cpp
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.cpp
@@ -6,27 +6,35 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int findMin(vector<int>& arr) {
+    int findMin(vector<int>& nums) {
         int low = 0;
-        int high = arr.size()-1;
+        int high = nums.size()-1;
         
         // run for at least 2 length
+        // while(low < high){
+        //     int mid = low + (high-low)/2;
+            
+        //     if(arr[mid] < arr[low]){
+        //         if(arr[mid-1] > arr[mid]){
+        //             low = mid;
+        //         }else{
+        //             high = mid-1;
+        //         }
+        //     }else{
+        //         if(arr[high] < arr[mid])low = mid+1;
+        //         else high = mid;
+        //     }
+            
+        // }
+        // return arr[low];//arr[high]
         while(low < high){
-            int mid = low + (high-low)/2;
-            
-            if(arr[mid] < arr[low]){
-                if(arr[mid-1] > arr[mid]){
-                    low = mid;
-                }else{
-                    high = mid-1;
-                }
-            }else{
-                if(arr[high] < arr[mid])low = mid+1;
-                else high = mid;
-            }
-            
+            int mid = low + (high - low)/2;
+            if(nums[mid] < nums[high]){
+                high = mid;
+            } 
+            else low = mid + 1;     
         }
-        return arr[low];//arr[high]
+        return nums[low];
     }
 };
 
