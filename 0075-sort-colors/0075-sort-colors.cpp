0e6b2478378640b19962b173975f70dd1379ec3e
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-23 18:35:12 +0530

    Time: 0 ms (100%), Space: 11.6 MB (13.62%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -7,25 +7,46 @@ public:
         // we have to do it in constant space because this is the in-place algo
 
         // better will be taking the count of the O,1 and 2 and inserting into the nums according to their respective count  0 to 2 -> O(2n) time and space->O(1)
-        int n = nums.size();
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
-        // O(n)
-        for(int i = 0; i < n; i++){
-            if(nums[i] == 0)count0++;
-            else if(nums[i] == 1)count1++;
-            else count2++;
-        }
-        // O(n)
-        for(int i = 0; i < n; i++){
-            if(i < count0)nums[i] = 0;
-            else if(i < count0+count1)nums[i] = 1;
-            else nums[i] = 2;
-        }
+        // int n = nums.size();
+        // int count0 = 0;
+        // int count1 = 0;
+        // int count2 = 0;
+        // // O(n)
+        // for(int i = 0; i < n; i++){
+        //     if(nums[i] == 0)count0++;
+        //     else if(nums[i] == 1)count1++;
+        //     else count2++;
+        // }
+        // // O(n)
+        // for(int i = 0; i < n; i++){
+        //     if(i < count0)nums[i] = 0;
+        //     else if(i < count0+count1)nums[i] = 1;
+        //     else nums[i] = 2;
+        // }
 
-        
-        // optimal will dutch algo in Time->O(n) and Space->O(1) 
+
+        // optimal will dutch algo in Time->O(n) and Space->O(1)
+
+        int n = nums.size();
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+
+        while(mid <= high){
+            if(nums[mid] == 1){
+                mid++;
+            }else if(nums[mid] == 0){
+                int temp = nums[low];
+                nums[low] = 0;
+                nums[mid] = temp;
+                low++; mid++;
+            }else{
+                int temp = nums[high];
+                nums[high] = 2;
+                nums[mid] = temp;
+                high--;
+            }
+        } 
 
     }
 };
