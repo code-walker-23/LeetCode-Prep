99ad0c89fd4ac4c8b51a98613d8917c77a58ca03
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-23 17:05:34 +0530

    Time: 0 ms (100%), Space: 11.6 MB (13.62%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,17 +2,30 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         // most brute force will be sort the array in O(nlogn).
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
         // we will be using the dutch national flag algorithm to get optimized soltuion in O(n)
         // we have to do it in constant space because this is the in-place algo
 
-        // better will be taking the count of the O,1 and 2 and inserting into the nums according to their respective count  0 to 2 -> O(n) sapce and time
+        // better will be taking the count of the O,1 and 2 and inserting into the nums according to their respective count  0 to 2 -> O(2n) time and space->O(1)
+        int n = nums.size();
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        // O(n)
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0)count0++;
+            else if(nums[i] == 1)count1++;
+            else count2++;
+        }
+        // O(n)
+        for(int i = 0; i < n; i++){
+            if(i < count0)nums[i] = 0;
+            else if(i < count0+count1)nums[i] = 1;
+            else nums[i] = 2;
+        }
 
+        
         // optimal will dutch algo in Time->O(n) and Space->O(1) 
-
-
-
-
 
     }
 };
