34dfff15d1c2f89b7be69261763409cefe26f74f
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-23 21:02:00 +0530

    Time: 0 ms (100%), Space: 28.1 MB (34.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,17 +2,33 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         //brute force will be O(n) both time and space
-        int n = nums.size();
-        unordered_map<int,int> mapi;
+        // int n = nums.size();
+        // unordered_map<int,int> mapi;
 
-        for(int i = 0; i < n; i++)mapi[nums[i]]++;
-        int ans = 0;
-        for(auto p : mapi){
-            if(p.second > n/2){
-                ans = p.first;
-                break;
+        // for(int i = 0; i < n; i++)mapi[nums[i]]++;
+        // int ans = 0;
+        // for(auto p : mapi){
+        //     if(p.second > n/2){
+        //         ans = p.first;
+        //         break;
+        //     }
+        // }
+        // return ans;
+
+        // Better in constant space
+
+        int n = nums.size();
+        int count = 1;
+        int element = nums[0];
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] == element)count++;
+            else count--;
+
+            if(count == 0){
+                element = nums[i+1];
             }
         }
-        return ans;
+        return element;
     }
 };
