b28b6a61c7ddc4ac5b3ef6caec6d1d64b23e0449
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-24 17:49:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count number of ways to reach the nth stair.
+    int countWays(int n) {
+        if(n == 2)return 2;
+        if(n == 1 )return 1;
+        
+        return (countWays(n-1)+countWays(n-2));
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    // taking total testcases
+    int t;
+    cin >> t;
+    while (t--) {
+        // taking stair count
+        int m;
+        cin >> m;
+        Solution ob;
+        cout << ob.countWays(m) << endl; // Print the output from our pre computed array
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
