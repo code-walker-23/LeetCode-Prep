a9c4b6241697df1b9eebe1127d04b04683780833
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-24 12:11:11 +0530

    Time: 0 ms (100%), Space: 97.5 MB (14.75%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        //brute force will be the O(n^2)
+        // int maxProfit = 0;
+        // for(int i = 0; i < prices.size(); i++){
+        //     for(int j = i+1; j < prices.size(); j++){
+        //         if(prices[j] > prices[i])maxProfit = max(maxProfit,prices[j]-prices[i]);
+        //     }
+        // }
+        // return maxProfit;
+
+        // optimized will be O(n)
+        
+        int maxProfit = 0;
+        int buy = prices[0];
+
+        for(int i = 1; i < prices.size(); i++){
+            if(prices[i] > buy)maxProfit = max(maxProfit,prices[i]-buy);
+            // i will buy now because it is minimum
+            else buy = prices[i];
+        }
+        // we will keep simple approach i will buy for minnimum and sell for max such that we will do 1 transaction that maximize the objectivity of the problem.
+        return maxProfit;
+    }
+};
