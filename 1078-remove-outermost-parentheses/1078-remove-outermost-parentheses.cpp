8bf76fc4b67893ffd1890ffd62008497b179e31f
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-30 23:33:03 +0530

    Time: 0 ms (100%), Space: 9 MB (24.97%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -2,29 +2,20 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
         int n = s.size();
-        stack<int> st;
-        //indices for prmitive decomposition
-        vector<int>indices;
+        stack<char> st;
         string ans;
         
+        int prev = 0;
         for(int i = 0; i < n; i++){
-            if(s[i] == '(')st.push(s[i]);
+            if(s[i] == '('){
+                if(i > prev)ans+=s[i];
+                st.push(s[i]);
+            }
             else{
                 st.pop();
-                if(st.empty())indices.push_back(i);
+                if(!st.empty())ans+=s[i];
+                else prev = i+1;
             }
-        }
-
-        int prev = -1;
-        for(int i = 0; i < indices.size(); i++){
-            int start = prev+2;
-            int end = indices[i]-1;
-
-            while(start <= end){
-                ans+=s[start];
-                start++;
-            }
-            prev = end+1;
         }
 
         return ans;
