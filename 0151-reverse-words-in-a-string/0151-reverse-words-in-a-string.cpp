190a4d15258f38b962e85292afa28ea278f7367f
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-31 18:47:11 +0530

    Time: 0 ms (100%), Space: 10.4 MB (53.72%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,22 +1,78 @@
 class Solution {
 public:
+    void reverseString(string &s, int low,int high){
+        while(low < high){
+            char temp = s[low];
+            s[low] = s[high];
+            s[high] = temp;
+            low++;
+            high--;
+        }
+    }
     string reverseWords(string s) {
         int n = s.size();
-
-        stringstream ss(s);
-        string word;
+        int l = 0;
+        int r = 0;
         string ans = "";
-        int i = 0;
 
-        while(ss >> word){
-            if(i == 0)ans = word;
-            else ans = word + " " + ans;
-            i++;
+        // first reverse whole string
+        reverseString(s,0,n-1);
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == ' ' && l < r){
+                reverseString(s,l,r-1);
+                s[r] = ' ';
+                r++;
+                l = r;
+            }else if(s[i] != ' '){
+                s[r] = s[i];
+                r++;
+            }
+        }
+
+        if(l < r){
+            reverseString(s,l,r-1);
+            ans = s.substr(0,r);
+        }else{
+            ans = s.substr(0,r-1);
         }
 
         return ans;
+
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         int n = s.size();
+
+//         stringstream ss(s);
+//         string word;
+//         string ans = "";
+//         int i = 0;
+
+//         while(ss >> word){
+//             if(i == 0)ans = word;
+//             else ans = word + " " + ans;
+//             i++;
+//         }
+
+//         return ans;
+//     }
+// };
 
 
 
