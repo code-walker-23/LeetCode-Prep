0f0dfb567b4e2d4963f42568ced11b3f454a1c4c
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-12-31 23:00:53 +0530

    Time: 0 ms (100%), Space: 8.1 MB (20.57%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -3,20 +3,43 @@ public:
     bool rotateString(string s, string goal) {
         if(s.size() != goal.size())return false;
         if(s == goal) return true;
-        // Brute Force
 
-        int rightMost = 0;
-        int leftMost = s.size()-1;
+        int n = s.size();
+        // efficient
+        // when we concatenate a string with itself we get all possible rotations
 
-        int rotation = 0;
+        s = s+s;
+        // we know goal is created by doing rotations now we can check it is a valid rotation or not
+        // for(int i = 1; i <= n; i++){
+        //     if(s.substr(i,n) == goal)return true;
+        // }
 
-        while(rotation < s.size()-1){
-            s = s.substr(1) + s[0];
-            if(s == goal)return true;
-            rotation++;
-        }
+        // return false;
+        // one liner
 
-        return false;
-
+        return s.find(goal) != string::npos;
     }
 };
+
+ // using stl
+// for(int i = 1; i < s.size(); i++){
+//     rotate(s.begin(),s.begin()+1,s.end());
+//     if(s == goal)return true;
+// }
+
+// return false;
+
+
+    // Brute Force
+    // int rightMost = 0;
+    // int leftMost = s.size()-1;
+
+    // int rotation = 0;
+    // O(n^2)
+    // while(rotation < s.size()-1){
+    //     s = s.substr(1) + s[0];
+    //     if(s == goal)return true;
+    //     rotation++;
+    // }
+
+    // return false;
