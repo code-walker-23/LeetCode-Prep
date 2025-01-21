5ef36d5a50d7f20db7b0175ad7ae001d469370fd
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-01-21 13:21:13 +0530

    Time: 0 ms (100%), Space: 18.2 MB (43.71%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -21,8 +21,36 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
         int m = image[0].size();
+        // dfs(sr,sc,n,m,color,image);
+        
+        queue<pair<int,int>>q;
+        int orgColor = image[sr][sc];
 
-        dfs(sr,sc,n,m,color,image);
+        q.push({sr,sc});
+        image[sr][sc] = color;
+
+        while(!q.empty()){
+            pair<int,int> p = q.front();
+            q.pop();
+
+            int i = p.first;
+            int j = p.second;
+
+            int del[4][2] = {{1,0},{-1,0},{0,-1},{0,1}};
+
+            for(int k = 0; k < 4; k++){
+                int row = i + del[k][0];
+                int col = j + del[k][1];
+
+                if(row < 0 || row >= n || col < 0 || col >= m)continue;
+
+                if(image[row][col] == orgColor && orgColor != color){
+                    image[row][col] = color;
+                    q.push({row,col});
+                }
+            }
+
+        }
 
         return image;
     }
