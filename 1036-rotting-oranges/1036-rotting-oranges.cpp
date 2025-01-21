183cab34cffc7216945e18bf445f23ae2e01323b
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-01-21 11:45:13 +0530

    Time: 0 ms (100%), Space: 16.8 MB (53.45%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
+//  we will be traversing level wise and the last level will be the min minutes to get all rotten
+// at max all will rotten then all will go to the queue
+// Space  -> O(n*m)
+// Time -> O(n*m +  n*m*4 + n*m) = O(n*m)
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        int count = 0;
+        int time = 0;
         queue<pair<pair<int,int>,int>>q;
+        int cntFresh = 0;
 
+        // n*m
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 2){
                     q.push({{i,j},0});
-                }
+                }else if(grid[i][j] == 1)cntFresh++;
             }
         }
 
+        int newRotten = 0;
+        // n*m
         while(!q.empty()){
             pair<pair<int,int>,int> p = q.front();
             q.pop();
@@ -22,8 +30,9 @@ public:
             int del[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
             int i = p.first.first;
             int j = p.first.second;
-            count = p.second;
+            time = p.second;
 
+            // n*m*4 time
             for(int k = 0; k < 4; k++){
                 int row = i + del[k][0]; 
                 int col = j + del[k][1];
@@ -33,20 +42,14 @@ public:
                 if(grid[row][col] == 1){
                     grid[row][col] = 2;
                     q.push({{row,col},p.second+1});
+                    newRotten++;
                 }
 
             }
 
         }
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
-            }
-        }
-
-        return count;
+        if(cntFresh != newRotten)return -1;
+        return time;
     }
 };
