5340a67ea68fd8a89af8418948f4e70e33e1c265
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-02-06 14:46:52 +0530

    Time: 0 ms (100%), Space: 17.6 MB (15.32%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
 
-    void dfs(int src,vector<vector<int>>&adj,vector<int>&vis,unordered_set<int> &A,unordered_set<int> &B,bool &isBipartite){
+    void dfs(int src, vector<vector<int>>& adj, vector<int>& vis, unordered_set<int>& A, unordered_set<int>& B, bool& notBipartite) {
         vis[src] = 1;
 
-        for(int nbrs : adj[src]){
-            if(A.find(src) != A.end()){
-                if(A.find(nbrs) != A.end()){
-                    isBipartite = false;
+        for (int nbr : adj[src]) {
+            if (A.count(src)) {  
+                if (A.count(nbr)) { 
+                    notBipartite = true;
                     return;
-                }else{
-                    B.insert(nbrs);
-                    if(!vis[nbrs]){
-                        dfs(nbrs,adj,vis,A,B,isBipartite);
-                    }
                 }
-            }else{
-                if(B.find(nbrs) != B.end()){
-                    isBipartite = false;
+                if (!vis[nbr]) {  
+                    B.insert(nbr);
+                    dfs(nbr, adj, vis, A, B, notBipartite);
+                    if (notBipartite) return;
+                }
+            } else { 
+                if (B.count(nbr)) {
+                    notBipartite = true;
                     return;
-                }else{
-                    A.insert(nbrs);
-                    if(!vis[nbrs]){
-                        dfs(nbrs,adj,vis,A,B,isBipartite);
-                    }
+                }
+                if (!vis[nbr]) {
+                    A.insert(nbr); 
+                    dfs(nbr, adj, vis, A, B, notBipartite);
+                    if (notBipartite) return;
                 }
             }
         }
@@ -31,18 +31,19 @@ public:
 
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n,0);
-        bool isBipartite = true;
-        unordered_set<int> A,B;
-        
-        for(int i = 0; i < n; i++){
-            if(!vis[i]){
+        vector<int> vis(n, 0);
+        bool notBipartite = false;
+        unordered_set<int> A, B;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
                 A.insert(i);
-                dfs(i,graph,vis,A,B,isBipartite);
-                if(!isBipartite)return false;
+                dfs(i, graph, vis, A, B, notBipartite);
+                if (notBipartite) return false;
             }
         }
 
-        return true;
+        return true; 
     }
+
 };
