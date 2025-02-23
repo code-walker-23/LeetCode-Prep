8274083775b7702dd3e8592d09d6fec97aad3c08
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-02-23 19:39:55 +0530

    Time: 0 ms (100%), Space: 9.5 MB (14.14%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void print(vector<int>& traversal, vector<vector<int>>& matrix, int srow, int erow, int scol, int ecol) {
+        // Traverse from left to right along the top row
+        for (int i = scol; i <= ecol; i++) 
+            traversal.push_back(matrix[srow][i]);
+
+        // Traverse from top to bottom along the right column
+        for (int i = srow + 1; i <= erow; i++) 
+            traversal.push_back(matrix[i][ecol]);
+
+        // Traverse from right to left along the bottom row, if there are rows remaining
+        if (srow < erow) {
+            for (int i = ecol - 1; i >= scol; i--) 
+                traversal.push_back(matrix[erow][i]);
+        }
+
+        // Traverse from bottom to top along the left column, if there are columns remaining
+        if (scol < ecol) {
+            for (int i = erow - 1; i > srow; i--) 
+                traversal.push_back(matrix[i][scol]);
+        }
+    }
+
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<int> traversal;
+        
+        int srow = 0;
+        int scol = 0;
+        int erow = row - 1;
+        int ecol = col - 1;
+        
+        while (srow <= erow && scol <= ecol) {
+            print(traversal, matrix, srow, erow, scol, ecol);
+            srow++; erow--;
+            scol++; ecol--;
+        }
+
+        return traversal;
+    }
+};
