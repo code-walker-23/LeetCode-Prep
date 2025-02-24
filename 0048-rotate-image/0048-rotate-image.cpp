410bb6f9f24cbef2054231c081b0ebb89eaa41c9
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-02-24 10:53:08 +0530

    Time: 0 ms (100%), Space: 10.6 MB (8.72%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        // we will always be using on the square matrix
         int n = matrix.size();
-        int m = matrix[0].size();
         // we can try to solve without or with lesser space than (n*m)
-        vector<vector<int>>res(n,vector<int>(m));
+        vector<vector<int>>res(n,vector<int>(n));
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
