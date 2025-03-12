c46405b63c7b0bb22bbeb35fa19d19809c832902
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-03-12 12:06:06 +0530

    Time: 0 ms (100%), Space: 97.2 MB (87.01%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -12,15 +12,26 @@ public:
 
         // optimized will be O(n)
         
-        int maxProfit = 0;
-        int buy = prices[0];
+        // int maxProfit = 0;
+        // int buy = prices[0];
 
-        for(int i = 1; i < prices.size(); i++){
-            if(prices[i] > buy)maxProfit = max(maxProfit,prices[i]-buy);
-            // i will buy now because it is minimum
-            else buy = prices[i];
+        // for(int i = 1; i < prices.size(); i++){
+        //     if(prices[i] > buy)maxProfit = max(maxProfit,prices[i]-buy);
+        //     // i will buy now because it is minimum
+        //     else buy = prices[i];
+        // }
+        // // we will keep simple approach i will buy for minnimum and sell for max such that we will do 1 transaction that maximize the objectivity of the problem.
+        // return maxProfit;
+        
+        // opposite of above solution
+        int n = (int)prices.size();
+        if(n < 2) return 0;
+        int maxPrize = 0, maxProfit = 0;
+        for(int i = n-1; i >= 0; i--) {
+            maxProfit = max(maxProfit, maxPrize - prices[i]);
+            maxPrize = max(maxPrize, prices[i]);
         }
-        // we will keep simple approach i will buy for minnimum and sell for max such that we will do 1 transaction that maximize the objectivity of the problem.
         return maxProfit;
+
     }
 };
