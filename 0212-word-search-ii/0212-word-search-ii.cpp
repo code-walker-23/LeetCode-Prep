7f3179d394ab14912184fcb847db77b5e6e3621d
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-04-20 19:54:13 +0530

    Time: 267 ms (78.79%), Space: 19 MB (84.23%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -0,0 +1,100 @@
+// we can also make this code more modular by making some helper methods in Node class
+class Node {
+public:
+    Node* arr[26]; // Array of pointers for each character (a-z)
+    bool flag; // Indicates if the node marks the end of a word
+    string word;
+
+    Node() {
+        fill(begin(arr), end(arr), nullptr); // Initialize all pointers to nullptr
+        flag = false; // Initialize flag to false
+        word = "";
+    }
+};
+
+class Trie{
+private:
+    Node* root;
+public :
+    Trie(){
+        root = new Node();
+    }
+
+    Node* getRoot(){
+        return root;
+    }
+    void insert(string str) {
+        Node* temp = root;
+        for (char ch : str) {
+            if (temp->arr[ch - 'a'] == nullptr) {
+                temp->arr[ch - 'a'] = new Node();
+            }
+            temp = temp->arr[ch - 'a'];
+        }
+        temp->flag = true;
+        temp->word = str;
+    }
+
+    bool search(string str) {
+        Node* temp = root;
+        for (char ch : str) {
+            if (temp->arr[ch - 'a'] == nullptr) return false;
+            temp = temp->arr[ch - 'a'];
+        }
+        return temp->flag;
+    }
+
+    bool startsWith(string str) {
+        Node* temp = root;
+        for (char ch : str) {
+            if (temp->arr[ch - 'a'] == nullptr) return false;
+            temp = temp->arr[ch - 'a'];
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    void dfs(int i,int j,Node* head,vector<vector<char>>&board,vector<string>&res){
+        if(head->word != ""){
+            res.push_back(head->word);
+            head->word = "";
+        }
+        if(i < 0 || i == board.size() || j < 0 || j == board[0].size())return;
+        if(board[i][j] == '$')return;
+        char ch = board[i][j];
+        if(head->arr[ch-'a'] == NULL)return;
+
+        int del[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+        Node* next = head->arr[ch-'a'];
+        board[i][j] ='$';
+        
+        for(int k = 0; k < 4; k++){
+            int nrow = del[k][0] + i;
+            int ncol = del[k][1] + j;
+
+            dfs(nrow,ncol,next,board,res);
+        }
+
+        board[i][j] = ch;
+    }
+    
+
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        // n*m*words.len*3^word[i].len -> TLE
+        int n = words.size();
+        Trie* obj = new Trie();
+        vector<string>res;
+
+        for(int i = 0; i < n; i++)obj->insert(words[i]);
+
+        for(int i = 0; i < board.size(); i++){
+            for(int j = 0; j < board[0].size(); j++){
+                dfs(i,j,obj->getRoot(),board,res);
+            }
+        }
+
+        return res;
+    }
+};
