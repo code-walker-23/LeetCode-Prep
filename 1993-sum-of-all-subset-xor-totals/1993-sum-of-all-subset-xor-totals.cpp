56e3698fa929390491f1295a7e1342261f98740d
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-05-05 12:55:38 +0530

    Time: 0 ms (100%), Space: 9.2 MB (34.84%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -16,6 +16,12 @@ public:
         return sum;
     }
     int subsetXORSum(vector<int>& nums) {
+        int total = 0;
+        for (int num : nums) {
+            total |= num;
+        }
+        return total * (1 << (nums.size() - 1)); // O(n)
+        // this 2^N 
         return sumXor(nums);
     }
 };
