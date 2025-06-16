b22a720053194f8ecb5b2679d381c26a78707ce3
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-06-16 14:08:22 +0530

    Time: 3 ms (100%), Space: 77.1 MB (48.32%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,26 +1,18 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        // Naive Approach -> O(n) and Space -> O(2*n)
-        // We can reduce the Space as well
         int n = nums.length;
-        int[] pos = new int[n/2];
-        int[] neg = new int[n/2];
-        int i = 0,j = 0;
-
-        for(int k = 0; k < n; k++){
-            if(nums[k] > 0){
-                pos[i++] = nums[k];
-            }else{
-                neg[j++] = nums[k];
-            }
-        } 
-
         int[] res = new int[n];
-        int h = 0;
+        int pos = 0;
+        int neg = 1;
 
-        for(i = 0; i < n/2; i++){
-            res[h++] = pos[i];
-            res[h++] = neg[i]; 
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0){
+                res[pos] = nums[i];
+                pos += 2;
+            }else{
+                res[neg] = nums[i];
+                neg += 2;
+            }
         }
 
         return res;
