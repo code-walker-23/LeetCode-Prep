8ac187a0fefb636838d188a14b2784bbcaed5189
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-06-17 13:44:12 +0530

    Time: 3 ms (30.8%), Space: 15.9 MB (100%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,55 +1,48 @@
 class Solution {
 public:
-//  we will be traversing level wise and the last level will be the min minutes to get all rotten
-// at max all will rotten then all will go to the queue
-// Space  -> O(n*m)
-// Time -> O(n*m +  n*m*4 + n*m) = O(n*m)
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+int orangesRotting(vector<vector<int>>& grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+    queue<pair<pair<int,int>,int>> q;
+    vector<vector<int>> visited(m,vector<int>(n,0));
+    int cntFresh = 0;
+    int newRotten = 0;
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            if(grid[i][j] == 2){
+                q.push({{i,j},0});
+                visited[i][j] = 2;
+            }
+            if(grid[i][j] == 1)cntFresh++;
+        }
+    }
 
-        int time = 0;
-        queue<pair<pair<int,int>,int>>q;
-        int cntFresh = 0;
+    int time = 0;
 
-        // n*m
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 2){
-                    q.push({{i,j},0});
-                }else if(grid[i][j] == 1)cntFresh++;
+    while(!q.empty()){
+        pair<pair<int,int>,int> front = q.front();
+        int r = front.first.first;
+        int c = front.first.second;
+        int t = front.second;
+        q.pop();
+
+        time = t;
+
+        int drow[] = {1,0,-1,0};
+        int dcol[] = {0,1,0,-1};
+
+        for(int i = 0; i < 4; i++){
+            int nrow = r + drow[i];
+            int ncol = c + dcol[i];
+
+            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && visited[nrow][ncol] != 2 && grid[nrow][ncol] == 1){
+                q.push({{nrow,ncol},1+t});
+                visited[nrow][ncol] = 2;
+                newRotten++;
             }
         }
-
-        int newRotten = 0;
-        // n*m
-        while(!q.empty()){
-            pair<pair<int,int>,int> p = q.front();
-            q.pop();
-
-            int del[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
-            int i = p.first.first;
-            int j = p.first.second;
-            time = p.second;
-
-            // n*m*4 time
-            for(int k = 0; k < 4; k++){
-                int row = i + del[k][0]; 
-                int col = j + del[k][1];
-
-                if(row < 0 || row >= n || col < 0 || col >= m)continue;
-
-                if(grid[row][col] == 1){
-                    grid[row][col] = 2;
-                    q.push({{row,col},p.second+1});
-                    newRotten++;
-                }
-
-            }
-
-        }
-
-        if(cntFresh != newRotten)return -1;
-        return time;
+    }
+    if(cntFresh != newRotten)return -1;
+    return time;
     }
 };
