2e252ff8bff497675c01d969f94e7961ba145d07
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-06-18 13:25:36 +0530

    Time: 106 ms (32.9%), Space: 47.4 MB (38.18%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -26,6 +26,10 @@ class Solution {
         q.add(new Pair<>(beginWord,1));
         m.remove(beginWord);
 
+        // At the worst case there will be all words in wordList (n) in the queue once 
+        // for each word -> Length is k average and k*26 after changing characters at all positions
+        // O(n*k*k*26 + nlogn) -> multiply k extra for stringbuilder 
+
         while(!q.isEmpty()){
             Pair<String,Integer>p = q.remove();
             String word = p.getKey();
@@ -40,10 +44,12 @@ class Solution {
                 for (char ch = 'a'; ch <= 'z'; ch++) {
                     if (word.charAt(i) == ch) continue;
 
+                    // (k) for copy all chars
                     StringBuilder sb = new StringBuilder(dummyWord);
-                    sb.setCharAt(i, ch);
-                    String str = sb.toString();
+                    sb.setCharAt(i, ch); // O(1)
+                    String str = sb.toString();// O(k)
 
+                    // each word will go only once in queue -> Overall this part is O(nlogn) -> n is words in wordList
                     if (m.containsKey(str)) {
                         q.add(new Pair<>(str, level + 1)); 
                         m.remove(str);
