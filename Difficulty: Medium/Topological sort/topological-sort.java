c949deb7cfea3355ca538801821ee17c1e3ceda9
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-06-19 19:32:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.java
+++ Difficulty: Medium/Topological sort/topological-sort.java
@@ -1,42 +1,61 @@
 class Solution {
-    public static void dfs(int src,int[] vis,ArrayList<ArrayList<Integer>> adj,ArrayList<Integer> topo){
-        vis[src] = 1;
+    // public static void dfs(int src,int[] vis,ArrayList<ArrayList<Integer>> adj,ArrayList<Integer> topo){
+    //     vis[src] = 1;
         
-        for(int nbr : adj.get(src)){
-            if(vis[nbr] == 0){
-                dfs(nbr,vis,adj,topo);
-            }
-        }
+    //     for(int nbr : adj.get(src)){
+    //         if(vis[nbr] == 0){
+    //             dfs(nbr,vis,adj,topo);
+    //         }
+    //     }
         
-        // after going to the deep push the src
-        topo.add(src);
-    }
+    //     // after going to the deep push the src
+    //     topo.add(src);
+    // }
     
+    // We will be using Kahn's algorithm
     public static ArrayList<Integer> topoSort(int V, int[][] edges) {
         ArrayList<ArrayList<Integer>>adj = new ArrayList<>();
         for(int i = 0; i < V; i++){
             adj.add(new ArrayList<>());
         }
         
+        int[] indegree = new int[V];
+        
         for(int[] edge : edges){
             int u = edge[0];
             int v = edge[1];
             
             adj.get(u).add(v);
+            indegree[v]++;
         }
-        
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < V; i++)if(indegree[i] == 0)q.add(i);
         ArrayList<Integer> topo = new ArrayList<>();
-        int[] vis = new int[V];
         
-        for(int i = 0; i < V; i++){
-            if(vis[i] == 0){
-                dfs(i,vis,adj,topo);
+        while(!q.isEmpty()){
+            int front = q.remove();
+            topo.add(front);
+            
+            for(int nbr : adj.get(front)){
+                indegree[nbr]--;
+                if(indegree[nbr] == 0)q.add(nbr);
             }
-        }
-        
-        Collections.reverse(topo);
+        }        
         
         return topo;
+        // ArrayList<Integer> topo = new ArrayList<>();
+        // int[] vis = new int[V];
+        
+        // for(int i = 0; i < V; i++){
+        //     if(vis[i] == 0){
+        //         dfs(i,vis,adj,topo);
+        //     }
+        // }
+        
+        // Collections.reverse(topo);
+        // return topo;
+        
         
     }
 }
