6a959a8dee4b0321c7a5d799af3a4dfa7b353749
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2025-10-25 16:12:13 +0530

    4020-LEXICOGRAPHICALLY-SMALLEST-PERMUTATION-GREATER-THAN-TARGET Stats: Time: 108 ms (8.8%), Space: 23.9 MB (23.95%) - LeetHub

--- 4020-lexicographically-smallest-permutation-greater-than-target/4020-lexicographically-smallest-permutation-greater-than-target.cpp
+++ 4020-lexicographically-smallest-permutation-greater-than-target/4020-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,48 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     string lexGreaterPermutation(string s, string target) {
         int n = target.size();
+        bool pointOfDiff = false;
         string ans = "";
-        sort(s.begin(), s.end());
-        
+        sort(s.rbegin(), s.rend());
+        if (s <= target) return "";
+
         for (int i = 0; i < n; i++) {
-            auto p = lower_bound(s.begin(), s.end(), target[i]);
-            if (p != s.end()) {
-                if(target[i] < (*p)){
-                    target[i] = *p;
-                    s.erase(p);
-                    sort(s.begin(), s.end());
-                    ans = target.substr(0, i + 1) + s;
-                    return ans;
-                }else{
-                     s.erase(p);
+            bool ok = true;
+            string temp = s;
+
+            for (int j = 0; j < i; j++) {
+                size_t pos = temp.find(target[j]);
+                if (pos != string::npos) {
+                    temp.erase(pos, 1);
+                } else {
+                    ok = false;
+                    break;
                 }
             }
-            else{
-                 if(i == 0)return "";
-                 else {
-                    int k = i-1;
-                    while(k >= 0){
-                        s.push_back(target[k]);
-                        sort(s.begin(),s.end());
-                        auto u = upper_bound(s.begin(),s.end(),target[k]);
-                        if(u != s.end()){
-                            target[k] = *u;
-                            s.erase(u);
-                            sort(s.begin(), s.end());
-                            ans = target.substr(0, k + 1) + s;
-                            return ans;
-                        }
-                        k--;
-                    }
-                    return "";
-                 }
+
+            if (ok) {
+                sort(temp.begin(), temp.end());
+                auto p = upper_bound(temp.begin(), temp.end(), target[i]);
+                if (p != temp.end()) {
+                    char nextChar = *p;
+                    temp.erase(p);
+                    string str = target.substr(0, i) + nextChar + temp;
+                    if (ans == "" || str < ans) ans = str;
+                }
             }
         }
-        if(s.size() == 0){
-            bool hasNext = next_permutation(target.begin(),target.end());
-            if(hasNext)return target;
-            return "";
+
+        if (ans == "") {
+            if (next_permutation(target.begin(), target.end()))
+                return target;
+            else
+                return "";
         }
         return ans;
     }
